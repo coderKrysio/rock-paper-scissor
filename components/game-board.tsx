33bfb36075fa968bb8d442be53a0708647d6a4dd
Anushree <anushreejaiswal.srn@gmail.com>
2024-01-05 01:25:26 +0530

    imported custom hooks and context

--- components/game-board.tsx
+++ components/game-board.tsx
@@ -1,35 +1,66 @@
 "use client";
 import { Computer } from "@/components/computer";
 import { Player } from "@/components/player";
-import { createContext, useState } from "react";
-
-type AppContextType = {
-    userSubmitted: boolean;
-    setUserSubmitted: React.Dispatch<React.SetStateAction<boolean>>;
-};
-
-const iAppContextState = {
-    userSubmitted: false,
-    setUserSubmitted: () => {},
-};
-
-export const AppContext = createContext<AppContextType>(iAppContextState);
+import { orbitron } from "@/lib/font";
+import { GameContext } from "@/lib/game-context";
+import { useGameBoard } from "@/lib/hooks";
+import Link from "next/link";
 
 export const GameBoard = () => {
-    const [userSubmitted, setUserSubmitted] = useState(false);
+    const {
+        playerChoice,
+        setPlayerChoice,
+        computerChoice,
+        setComputerChoice,
+        matchStatus,
+        setMatchStatus,
+        userSubmitted,
+        setUserSubmitted,
+        handleRefresh,
+    } = useGameBoard();
 
     return (
-        <div className="flex w-full min-h-screen flex-col items-center justify-start p-4">
-            <div className="w-fit text-5xl text-center text-transparent font-bold mb-16 bg-gradient-to-r from-[#cc11f0] via-[#ff008d] to-[#6300ff] bg-clip-text select-none">
+        <div className="flex w-full h-full flex-col items-center justify-start gap-16 p-4">
+            <Link
+                href={"/"}
+                className="w-fit text-3xl text-center text-transparent font-bold bg-gradient-to-r from-[#ff008d] to-[#6300ff] bg-clip-text select-none"
+            >
                 Rock Paper Scissor
-            </div>
+            </Link>
 
-            <AppContext.Provider value={{ userSubmitted, setUserSubmitted }}>
-                <div className="flex items-start justify-center gap-20 select-none">
+            <GameContext.Provider
+                value={{
+                    userSubmitted,
+                    setUserSubmitted,
+                    playerChoice,
+                    setPlayerChoice,
+                    computerChoice,
+                    setComputerChoice,
+                    matchStatus,
+                    setMatchStatus,
+                }}
+            >
+                <div className="flex flex-wrap items-start justify-center gap-20 select-none">
                     <Player />
                     <Computer />
                 </div>
-            </AppContext.Provider>
+            </GameContext.Provider>
+
+            {matchStatus !== "" && (
+                <div>
+                    <div
+                        className={`text-4xl text-center text-[#00aba9] font-bold max-[465px]:text-3xl max-[397px]:text-2xl ${orbitron.className}`}
+                    >
+                        {matchStatus}
+                    </div>
+                    <button
+                        className="underline underline-offset-4 text-lg text-[#f58b57] text-center w-full mt-5"
+                        onClick={handleRefresh}
+                    >
+                        Restart
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
